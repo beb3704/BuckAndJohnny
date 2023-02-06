32ba579af43ff26115a1e29c9dd802de31306f94
Ike Melancon <ijm0117@p2es.com>
2023-02-05 21:05:57 -0600

    f

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,9 +1,9 @@
 <template>
     <div class="lg:p-10 max-w-7xl m-auto">
-        <NuxtImg :src="blog.imageUrl" format="webp" sizes="sm:100vw md:100vw lg:50vw" quality="70"
+        <NuxtImg v-if="blog" :src="blog.imageUrl" format="webp" sizes="sm:100vw md:100vw lg:50vw" quality="70"
             class="object-cover m-auto w-full" alt="blog image"></NuxtImg>
         <h1 class="bg-backgroundLight p-4 text-2xl">{{ blog?.title }}</h1>
-        <div v-html="blog.body" class="bg-backgroundLight p-4">
+        <div v-if="blog" v-html="blog.body" class="bg-backgroundLight p-4">
 
         </div>
     </div>
