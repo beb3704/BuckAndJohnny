df35c9d2f4dc42a0cf416f471d71ce0bfae44102
Ike Melancon <ijm0117@p2es.com>
2023-02-11 21:38:04 -0600

    wdf

--- components/media/imagecarousel.vue
+++ components/media/imagecarousel.vue
@@ -2,16 +2,11 @@
     <swiper-container slides-per-view="auto" autoplay-delay="3000"
         class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <swiper-slide v-for="href in slides" class="grid justify-center">
-            <NuxtImg format="webp" quality="70" class="object-cover w-full h-full" :src="href">
+            <NuxtImg format="webp" quality="70" class="object-cover w-full h-full" :src="$img(href)">
             </NuxtImg>
         </swiper-slide>
     </swiper-container>
 </template>
-<!-- 
-navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-}, -->
 
 <script  setup lang="ts">
 import { register } from 'swiper/element/bundle';
@@ -22,11 +17,4 @@ const props = defineProps({
     slides: { type: Array as PropType<Array<string>>, required: true },
 })
 
-useSeoMeta({
-    title: "A Better title",
-    ogTitle: "A Good Title",
-    description: "A dank page",
-    ogDescription: "A dank Page",
-    ogImage: '/logo.png'
-});
 </script>
--- components/media/logocarousel.vue
+++ components/media/logocarousel.vue
@@ -1,49 +1,9 @@
 <template>
     <swiper-container slides-per-view="auto" autoplay-delay="3000"
         class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-
-        <swiper-slide class="grid justify-center">
-            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px"
-                src="https://splashdownstorage.blob.core.windows.net/logos/alaglas.png"></NuxtImg>
+        <swiper-slide class="grid justify-center" v-for="href in slides">
+            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px" :src="$img(href)"></NuxtImg>
         </swiper-slide>
-
-        <swiper-slide class="grid justify-center">
-            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px"
-                src="https://splashdownstorage.blob.core.windows.net/logos/aquachek.png"></NuxtImg>
-        </swiper-slide>
-
-        <swiper-slide class="grid justify-center">
-            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px"
-                src="https://splashdownstorage.blob.core.windows.net/logos/barrierreef.png"></NuxtImg>
-        </swiper-slide>
-
-        <swiper-slide class="grid justify-center">
-            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px"
-                src="https://splashdownstorage.blob.core.windows.net/logos/bluehawaiian.png"></NuxtImg>
-        </swiper-slide>
-
-
-        <swiper-slide class="grid justify-center">
-            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px"
-                src="https://splashdownstorage.blob.core.windows.net/logos/cardinal.png"></NuxtImg>
-        </swiper-slide>
-
-
-        <swiper-slide class="grid justify-center">
-            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px"
-                src="https://splashdownstorage.blob.core.windows.net/logos/century.png"></NuxtImg>
-        </swiper-slide>
-
-        <swiper-slide class="grid justify-center">
-            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px"
-                src="https://splashdownstorage.blob.core.windows.net/logos/claquavations.png"></NuxtImg>
-        </swiper-slide>
-
-        <swiper-slide class="grid justify-center">
-            <NuxtImg format="webp" quality="100" sizes="sm:150px md:250px lg:350px"
-                src="https://splashdownstorage.blob.core.windows.net/logos/dolphin.png"></NuxtImg>
-        </swiper-slide>
-
     </swiper-container>
 </template>
 
@@ -54,6 +14,14 @@ import { register } from 'swiper/element/bundle';
 register();
 
 let slides = [
+    'https://splashdownstorage.blob.core.windows.net/logos/alaglas.png',
+    'https://splashdownstorage.blob.core.windows.net/logos/aquachek.png',
+    'https://splashdownstorage.blob.core.windows.net/logos/barrierreef.png',
+    'https://splashdownstorage.blob.core.windows.net/logos/bluehawaiian.png',
+    'https://splashdownstorage.blob.core.windows.net/logos/cardinal.png',
+    'https://splashdownstorage.blob.core.windows.net/logos/century.png',
+    'https://splashdownstorage.blob.core.windows.net/logos/claquavations.png',
+    'https://splashdownstorage.blob.core.windows.net/logos/dolphin.png',
     'https://splashdownstorage.blob.core.windows.net/logos/fluidra.png',
     'https://splashdownstorage.blob.core.windows.net/logos/haviland.png',
     'https://splashdownstorage.blob.core.windows.net/logos/hayward.png',
@@ -76,15 +44,4 @@ let slides = [
     'https://splashdownstorage.blob.core.windows.net/logos/tallman.png',
     'https://splashdownstorage.blob.core.windows.net/logos/tara.png'
 ]
-
-
-
-
-useSeoMeta({
-    title: "A Better title",
-    ogTitle: "A Good Title",
-    description: "A dank page",
-    ogDescription: "A dank Page",
-    ogImage: '/logo.png'
-});
 </script>
