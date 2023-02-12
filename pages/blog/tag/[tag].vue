b1150294b6bc25447142f482d74677c9e6524818
Ike Melancon <ijm0117@p2es.com>
2023-02-12 12:33:24 -0600

    sd

--- pages/blog/tag/[tag].vue
+++ pages/blog/tag/[tag].vue
@@ -4,12 +4,13 @@
         </MarlinSubhead>
         <div class="grid max-w-7xl md:gap-10 justify-center lg:grid-cols-2 md:p-5 m-auto">
 
-            <article v-for="blog in blogs" class="bg-backgroundDark relative flex flex-col shadow-md">
+            <article v-for="blog in blogs"
+                class="from-backgroundLight bg-gradient-to-t to-[#fff] relative flex flex-col shadow-md rounded-lg overflow-hidden">
                 <NuxtImg :src="blog.imageUrl" format="webp" width="400px" height="250px" sizes="sm:400px" quality="70"
-                    class="object-cover object-bottom w-full h-72 rounded-t relative z-0 clip" loading="lazy"
+                    class="object-cover object-bottom w-full h-72 rounded-lg  relative z-0 clip" loading="lazy"
                     alt="blog image">
                 </NuxtImg>
-                <div class="lg:p-4 relative z-10 bg-[#fff] -mt-32 m-4 shadow-sm h-full grid p-1">
+                <div class="lg:p-4 relative z-10 bg-[#fff] -mt-32 m-4 shadow-sm h-full grid  rounded">
                     <div>
                         <div class="p-4  font-bold text-titles">
                             {{ blog.title }}
@@ -18,8 +19,8 @@
                             {{ blog.excerpt }}
                         </div>
                     </div>
-                    <NuxtLink :to="`/blog/${blog.seoSlug}`" class="self-end  flex justify-self-end -mr-[1px]">
-                        <div class="bg-[#009AB3] w-8 clipMore">
+                    <NuxtLink :to="`/blog/${blog.seoSlug}`" class="self-end  flex justify-self-end">
+                        <div class="bg-[#009AB3] w-8 clipMore -mr-[1px]">
 
                         </div>
                         <div class="font-bold text-center uppercase py-2 bg-[#009AB3] text-[#fff] w-36">
