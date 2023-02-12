1db6da7086b633d87fff1b52b88a87a623f4ad8d
Ike Melancon <ijm0117@p2es.com>
2023-02-12 13:12:53 -0600

    sdf

--- pages/blog/tag/[tag].vue
+++ pages/blog/tag/[tag].vue
@@ -7,7 +7,7 @@
             <article v-for="blog in blogs"
                 class="from-backgroundLight bg-gradient-to-t to-[#fff] relative flex flex-col shadow-md rounded-lg overflow-hidden">
                 <NuxtImg :src="blog.imageUrl" format="webp" width="400px" height="250px" sizes="sm:400px" quality="70"
-                    class="object-cover object-bottom w-full h-72 rounded-lg  relative z-0 clip" loading="lazy"
+                    class="object-cover object-bottom w-full h-72 rounded-t-lg  relative z-0 clip" loading="lazy"
                     alt="blog image">
                 </NuxtImg>
                 <div class="lg:p-4 relative z-10 bg-[#fff] -mt-32 m-4 shadow-sm h-full grid  rounded">
@@ -61,7 +61,7 @@ function getFilteredBlog() {
 
 </script>
 
-<style>
+<style scoped>
 .clip {
     clip-path: polygon(100% 0, 100% 100%, 0 70%, 0 0);
 
