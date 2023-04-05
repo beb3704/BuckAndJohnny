cb6ab7572b146f5287201f98c13e1756a2ef92c6
Blake Berard <bb@theroomisred.com>
2023-04-05 10:46:35 -0500

    asdf

--- pages/special-events.vue
+++ pages/special-events.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
     <Bnjnav></Bnjnav>
-    <SubHeader title="special events" sub-title="lovely options for your next gathering">
-      <div class="text-textLight grid grid-cols-2 p-2 gap-10 m-auto" style="width: 375px">
-      </div>
+    <SubHeader
+      title="special events"
+      sub-title="lovely options for your next gathering"
+    >
+      <div
+        class="text-textLight grid grid-cols-2 p-2 gap-10 m-auto"
+        style="width: 375px"
+      ></div>
     </SubHeader>
     <div class="grid lg:grid-cols-2 md:grid-cols-2 m-auto max-w-6xl">
-      <div class="grid grid-cols-[1fr,min-content] py-6 md:px-4 px-8 content-start text-center">
+      <div
+        class="grid grid-cols-[1fr,min-content] py-6 md:px-4 px-8 content-start text-center"
+      >
         <div class="p-2 uppercase font-bold">
-          <NuxtImg format="webp" quality="70" sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw" src="glassroom.jpg"
-            class="h-auto w-full" alt="The Glass Room Interio0r"></NuxtImg>
+          <NuxtImg
+            format="webp"
+            quality="70"
+            sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw"
+            src="glassroom.jpg"
+            class="h-auto w-full"
+            alt="The Glass Room Interio0r"
+          ></NuxtImg>
           <br />
           <h2>The Glass Room</h2>
-          <span class="text-darkRed">An Elegantly Appointed Space | accommodates 400</span>
+          <span class="text-darkRed"
+            >An Elegantly Appointed Space | accommodates 400</span
+          >
         </div>
         <div class="p-2 col-span-full">
           Imagine… exposed brick walls, a high ceiling and wall of windows from
@@ -23,22 +38,36 @@
           bars. Our amazing staff will customize your event per request and
           provide top-shelf service throughout.
         </div>
-
       </div>
-      <a href="yourfile.pdf" download>
-        <NuxtImg format="webp" quality="70" sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw" src="download.png"
-          class="h-auto w-80 m-auto" alt="The Upstairs Room Interio0r"></NuxtImg>
+      <a href="2023 Glassroom Package.pdf" download>
+        <NuxtImg
+          format="webp"
+          quality="70"
+          sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw"
+          src="download.png"
+          class="h-auto w-80 m-auto"
+          alt="The Upstairs Room Interio0r"
+        ></NuxtImg>
       </a>
 
       <div
-        class="grid grid-cols-[1fr,min-content] py-6 md:px-4 px-8 content-start text-center md:col-start-2 md:row-start-1 ">
+        class="grid grid-cols-[1fr,min-content] py-6 md:px-4 px-8 content-start text-center md:col-start-2 md:row-start-1"
+      >
         <div class="p-2 uppercase font-bold">
-          <NuxtImg format="webp" quality="70" sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw" src="upstairs.jpg"
-            class="h-auto w-full" alt="The Upstairs Room Interio0r"></NuxtImg>
+          <NuxtImg
+            format="webp"
+            quality="70"
+            sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw"
+            src="upstairs.jpg"
+            class="h-auto w-full"
+            alt="The Upstairs Room Interio0r"
+          ></NuxtImg>
           <br />
           <h2>Upstairs</h2>
 
-          <span class="text-darkRed">A more casual event space | accommodates 50</span>
+          <span class="text-darkRed"
+            >A more casual event space | accommodates 50</span
+          >
         </div>
         <div class="p-2 col-span-full">
           Upstairs at Buck and Johnny's has been host to a long list of event
@@ -50,16 +79,25 @@
           alongside a dedicated server team.
         </div>
       </div>
-      <a href="yourfile2.pdf" download>
+      <!-- <a href="yourfile2.pdf" download>
         <NuxtImg format="webp" quality="70" sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw" src="download.png"
           class="h-auto w-80 m-auto" alt="The Upstairs Room Interio0r"></NuxtImg>
       </a>
-
+ -->
     </div>
-    <div class=" text-center bg-lightRed mt-10 p-4 py-8">
-      <h6 class="text-[#fff] text-3xl md:text-5xl uppercase font-bold tracking-wide">request a booking</h6>
+
+    <br />
+    <br />
+    <div class="text-center bg-lightRed mt-10 p-4 py-8">
+      <h6
+        class="text-[#fff] text-3xl md:text-5xl uppercase font-bold tracking-wide"
+      >
+        request a booking
+      </h6>
     </div>
     <contact></contact>
+    <br />
+    <br />
     <br />
     <br />
     <br />
